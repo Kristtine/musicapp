8a4c68ec18969f71c6e960daa82b64e824daa753
Chisom C Uzoechina <114270713+Kristtine@users.noreply.github.com>
2022-10-29 06:23:21 +0100

    Update models.py

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from datetime import datetime
+today = datetime.today
 
 # Create your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=400)
+    last_name = models.CharField(max_length=400)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.first_name
+
+class Song (models.Model):
+    title = models.CharField(max_length=400)
+    date_released = models.DateField(default=datetime.today) 
+    likes = models.IntegerField()
+    Artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Lyric(models.Model):
+    content =  models.TextField()
+    song_id= models.ForeignKey(Song, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
